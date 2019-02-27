7dd38db1b27605d6cafb148bed674c6c2dab1ed2
John Jacobs <jjacobs@xs4all.nl>
2019-02-27 23:10:34 +0100

    Split emacs configuration between init.el and org_init.org. Modified collect.sh accordingly.

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 cp ~/.vimrc ~/Repos/config/
-#cp ~/.emacs ~/Repos/config/
 cp ~/.emacs.d/init.el ~/Repos/config/
+cp ~/.emacs.d/org_init.org ~/Repos/config/
 cp ~/.bashrc ~/Repos/config/
 cp ~/.config/conky/conky.conf ~/Repos/config/
 cp ~/.config/autostart/conky.desktop ~/Repos/config/
--- init.el
+++ init.el
@@ -26,9 +26,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; UI settings
-(setq default-cursor-type '(bar . 3))
-
 ;; Setup package repositories
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -38,87 +35,10 @@
 	     '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
+;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-;; Org-mode
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-;; General modes
-
-; ibuffer - use ibuffer for buffer list (C-x C-b)
-(defalias 'list-buffers 'ibuffer)
-
-;; make buffer switch command show suggestions (C-x b)
-(setq ido-enable-flex-maiching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; auto-complete
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-;; ivy, counsel and swiper
-(use-package swiper
-  :ensure t
-  )
-
-(use-package counsel
-  :ensure t
-  )
-
-(use-package ivy
-  :ensure t
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq enable-recursive-minibuffers t)
-    (global-set-key "\C-s" 'swiper)
-    (global-set-key (kbd "C-c C-r") 'ivy-resume)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    (global-set-key (kbd "<f1> l") 'counsel-find-library)
-    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (global-set-key (kbd "C-c g") 'counsel-git)
-    (global-set-key (kbd "C-c j") 'counsel-git-grep)
-    (global-set-key (kbd "C-c k") 'counsel-ag)
-    (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-    ))
-
-;; color theme
-(load-theme 'wombat)
-
-(global-hl-line-mode)
-(set-face-background hl-line-face "gray20")
-
-;; linum mode
-(global-linum-mode 1)
-
-;; Evil mode
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-
+;; Load additional configuration
+(org-babel-load-file (expand-file-name "~/.emacs.d/org_init.org"))
