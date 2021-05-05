c0c20f33abb922c3de7eba44065ae9fdab00e0f7
John Jacobs <jjacobs@xs4all.nl>
2021-05-05 18:58:08 +0200

    Clear files before copy

--- collect.sh
+++ collect.sh
@@ -2,10 +2,14 @@
 cp ~/.vimrc ~/Repos/config/
 cp ~/.bashrc ~/Repos/config/
 cp ~/.gitconfig ~/Repos/config/
+rm ~/Repos/config/bin/*
 cp ~/bin/git_diff_wrapper ~/Repos/config/bin/
 cp ~/bin/load_shared ~/Repos/config/bin/
 cp ~/bin/tml ~/Repos/config/bin/
+rm -rf ~/Repos/config/alacritty
 cp -R ~/.config/alacritty ~/Repos/config
+rm -rf ~/Repos/config/conky
 cp -R ~/.config/conky ~/Repos/config
+rm -rf ~/Repos/config/neofetch
 cp -R ~/.config/neofetch ~/Repos/config
 
